a1ef621b63936679cdd081a594e6d82de0807d84
Victoria Atanasova <victoria.atanasova@netreact.com>
2020-10-02 17:28:31 +0300

    render products

---
+++ frontend/src/productsData.js
@@ -0,0 +1,29 @@
+export default {
+    products:[{
+        name: "Slim Shirt",
+        category: "Shirts",
+        image: "/images/d1.jpg",
+        price: 60,
+        brand: "Nike",
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        name: "Fit Shirt",
+        category: "Shirts",
+        image: "/images/d1.jpg",
+        price: 50,
+        brand: "Nike",
+        rating: 4.2,
+        numReviews: 5
+    },
+    {
+        name: "Best Pants",
+        category: "Pants",
+        image: "/images/d1.jpg",
+        price: 70,
+        brand: "Nike",
+        rating: 3,
+        numReviews: 12
+    }]
+}

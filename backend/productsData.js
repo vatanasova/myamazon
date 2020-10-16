9594ec0ab5013555c72ef4f82ced88b5bd5392f5
Victoria Atanasova <victoria.atanasova@netreact.com>
2020-10-16 16:02:38 +0300

    add to cart

--- backend/productsData.js
+++ backend/productsData.js
@@ -7,7 +7,8 @@ export default {
         price: 60,
         brand: "Nike",
         rating: 4.5,
-        numReviews: 10
+        numReviews: 10,
+        countInStock: 0,
     },
     {
         _id: "2",
@@ -17,7 +18,8 @@ export default {
         price: 50,
         brand: "Nike",
         rating: 4.2,
-        numReviews: 5
+        numReviews: 5,
+        countInStock: 1,
     },
     {
         _id: "3",
@@ -27,6 +29,7 @@ export default {
         price: 70,
         brand: "Nike",
         rating: 3,
-        numReviews: 12
+        numReviews: 12,
+        countInStock: 6,
     }]
 }

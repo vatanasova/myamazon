3599be8f2d00249280f222b4f981b05257188707
Victoria Atanasova <victoria.atanasova@netreact.com>
2020-10-16 15:07:21 +0300

    add redux to details page

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,15 @@ import express from 'express';
 import productsData from './productsData';
 const app = require('express')();
 
+app.get("/api/products/:id", (req, res) => {
+    const productId = req.params.id;
+    const product = productsData.products.find(x => x._id === productId);
+    if (product) 
+        res.send(product);
+    else
+        res.status(404).send({ msg: "Product Not Found." });
+});
+
 app.get("/api/products", (req, res) => {
     res.send(productsData.products);
 });

966208c882aff18aec5a08661f116abb2a304ede
Victoria Atanasova <victoria.atanasova@netreact.com>
2020-10-19 16:48:20 +0300

    shopping cart screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,14 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { productListReducer, productDetailsReducer } from './reducers/productReducers';
 import thunk from 'redux-thunk';
+import Cookie from 'js-cookie';
 import { cartReducer } from './reducers/cartReducers';
 
-const initialState = {};
+const cartItems = Cookie.getJSON('cartItems') || [];
+
+const initialState = {
+    cart: { cartItems },
+  };
 
 const reducer = combineReducers({
     productList: productListReducer,

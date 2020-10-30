c8e16797b5ab82f27b7980a195faba96e3c7a9b9
Victoria Atanasova <victoria.atanasova@netreact.com>
2020-10-30 17:36:08 +0200

    sign in user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,13 @@ import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import { useSelector } from 'react-redux';
 
 function App() {
+
+    const userSignin = useSelector((state) => state.userSignin);
+    const { userInfo } = userSignin;
 
     const openMenu = () => {
         document.querySelector(".sidebar").classList.add("open");
@@ -28,7 +33,22 @@ function App() {
                     </div>
                     <div className="header-links">
                         <a href="cart.html">Cart</a>
-                        <Link to="/signin">Sign In</Link>
+                        {userInfo ? (
+                            <Link to="/profile">{userInfo.name}</Link>
+                        ) : (
+                            <Link to="/signin">Sign In</Link>
+                        )}
+                        {userInfo && userInfo.isAdmin && (
+                        <div className="dropdown">
+                            <a href="#">Admin</a>
+                            <ul className="dropdown-content">
+                                <li>
+                                    <Link to="/orders">Orders</Link>
+                                    <Link to="/products">Products</Link>
+                                </li>
+                            </ul>
+                        </div>
+                        )}
                     </div>
                 </header>
                 <aside className="sidebar">
@@ -46,6 +66,7 @@ function App() {
                 <main className="main">
                     <div className="content">
                         <Route path="/signin" component={SigninScreen} />
+                        <Route path="/register" component={RegisterScreen} />
                         <Route path="/product/:id" component={ProductScreen} />
                         <Route path="/cart/:id?" component={CartScreen} />
                         <Route path="/" exact={true} component={HomeScreen} />
